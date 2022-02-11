cd69e6e46baebaeb0cad4e462437bcccff92ee65
Akin <akinkavak1@gmail.com>
2022-02-12 00:04:40 +0300

    Routing components

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,7 +2,33 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 
 function NavBar() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+    <div className="navbar">
+      <NavLink
+        to="/movies"
+        exact
+      >
+        Movies
+      </NavLink>
+      <NavLink
+        to="/directors"
+        exact
+      >
+        Directors
+      </NavLink>
+      <NavLink
+        to="/actors"
+        exact
+      >
+        Actors
+      </NavLink>
+      <NavLink
+        to="/"
+      >
+        Home
+      </NavLink>
+    </div>
+  );
 }
 
 export default NavBar;
